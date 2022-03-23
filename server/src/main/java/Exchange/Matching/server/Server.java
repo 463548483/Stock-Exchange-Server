4b9bb1323f6e0188ff6d0c2d1aa397d47c1b1281
zw255 <zw255@duke.edu>
2022-03-23 19:19:54 -0400

    add

--- server/src/main/java/Exchange/Matching/server/Server.java
+++ server/src/main/java/Exchange/Matching/server/Server.java
@@ -10,7 +10,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+
 import javax.xml.parsers.*;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Server {
     private ServerSocket socket;
@@ -46,33 +54,74 @@ public class Server {
             try{
                 Trans=new DataInputStream(socket.getInputStream());
                 long fileLen=Trans.readLong();
-                
-                File file=new File(Integer.toString(task_id));
-                fileout=new FileOutputStream(file);
-                byte[] bytes=new byte[1024];
-                for(;;){
-                    int n=Trans.read(bytes,0,bytes.length);
-                    if(n==-1){
-                        break;
-                    }
-                    fileout.write(bytes,0,n);
-                    System.out.print(bytes);
-                    fileout.flush();
-                }
-                System.out.println("Received file"+Integer.toString(Server.task_id)+Long.toString(fileLen));
 
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                Document doc=builder.parse(Trans);
+                switch (doc.getFirstChild().getNodeName()){
+                    case "create" :
+                        create_parse(doc.getFirstChild());
+                    case "transactions":
+                        transactions_parse(doc.getFirstChild());
+                }
+                //printNode(doc, 1);
+                NodeList create_list=doc.getElementsByTagName("create");
+                NodeList account_list=doc.getElementsByTagName("account");
+                NodeList symbol_list=doc.getElementsByTagName("symbol");
+
+                for (int i = 0; i < account_list.getLength(); i++) {
+                    Node account = account_list.item(i);
+                    System.out.println("Name: "+account.getNodeName()+" Value: "+account.getNodeValue()+" Type: "+account.getNodeType());
+                    NamedNodeMap attrs= account.getAttributes();
+                    for(int j=0;j<attrs.getLength();j++){
+                        Node x=attrs.item(j);
+                        System.out.println(x.getNodeName()+" "+x.getNodeValue()+" "+x.getNodeType());
+                    }  
+                }
+                for (int i = 0; i < symbol_list.getLength(); i++) {
+                    Node symbol = symbol_list.item(i);
+                    System.out.println("Name: "+symbol.getNodeName()+" Value: "+symbol.getNodeValue()+" Type: "+symbol.getNodeType());
+                    NamedNodeMap attrs= symbol.getAttributes();
+                    for(int j=0;j<attrs.getLength();j++){
+                        Node x=attrs.item(j);
+                        System.out.println(x.getNodeName()+" "+x.getNodeValue()+" "+x.getNodeType());
+                    }  
+                }
             }catch(Exception e){
                 e.printStackTrace();
             }finally{
                 try{
-                    if(fileout!=null){
-                        fileout.close();
-                    }
-                    if (Trans!=null){
-                        Trans.close();
-                    }
+
                     socket.close();
                 }catch(Exception e){}
+            }
+        }
+    }
+
+    void create_parse(Node n, int indent) {
+        for (int i = 0; i < indent; i++) {
+            System.out.print(' ');
+        }
+        switch (n.getNodeType()) {
+        case Node.DOCUMENT_NODE: // Document节点
+            System.out.println("Document: " + n.getNodeName());
+            break;
+        case Node.ELEMENT_NODE: // 元素节点
+            System.out.println("Element: " + n.getNodeName());
+            break;
+        case Node.TEXT_NODE: // 文本
+            System.out.println("Text: " + n.getNodeName() + " = " + n.getNodeValue());
+            break;
+        case Node.ATTRIBUTE_NODE: // 属性
+            System.out.println("Attr: " + n.getNodeName() + " = " + n.getNodeValue());
+            break;
+        default: // 其他
+            System.out.println("NodeType: " + n.getNodeType() + ", NodeName: " + n.getNodeName());
+        }
+        for (Node child = n.getFirstChild(); child != null; child = child.getNextSibling()) {
+            switch (child.getNodeName()){
+                case "account":
+                case "symbol"
             }
         }
     }
