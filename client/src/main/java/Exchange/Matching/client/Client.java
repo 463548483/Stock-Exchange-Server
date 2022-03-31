efe551fe642a41117961d388c7a9f8d1ee742cf7
zw255 <zw255@duke.edu>
2022-03-31 13:37:22 -0400

    te

--- client/src/main/java/Exchange/Matching/client/Client.java
+++ client/src/main/java/Exchange/Matching/client/Client.java
@@ -8,6 +8,7 @@ import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -50,7 +51,18 @@ public class Client extends Socket {
     }
 
     public void receive() throws IOException {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+        try {
+            // DataInputStream dis=new DataInputStream(socket.getInputStream());
+            // File file = new File("response.xml");
+            // FileOutputStream fos=new FileOutputStream(file);
+            // byte[] bytes = new byte[1024];
+            // int length = 0;
+            // while((length = dis.read(bytes, 0, bytes.length)) != -1) {
+            //     fos.write(bytes, 0, length);
+            //     fos.flush();
+            // }
+            // fos.close();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             while (true) {
                 String str = bufferedReader.readLine();
                 if (str == null) {
